961d60e5deb2774f34bc49e0f5d30e94cc72e980
Nikhita Zulka <41903530+nikx12@users.noreply.github.com>
2024-01-18 04:10:25 +0000

    jest setup and basic test case

---
+++ reactTest/src/sum/sum.js
@@ -0,0 +1,4 @@
+
+export default function Sum(num1, num2) {
+    return (num1+num2);
+}
---
+++ reactTest/src/sum/sum.test.js
@@ -0,0 +1,5 @@
+import Sum from "./sum";
+
+test("Testing for sum function", ()=> {
+    expect(Sum(10,22)).toBe(32)
+})
